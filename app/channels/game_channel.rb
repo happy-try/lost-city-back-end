9532ba2ac7c3d454db0c2d2eb587419769bd570b
mxchenxiaodong <943000653@qq.com>
2021-01-19 21:38:14 +0800

    初版

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,17 +1,8 @@
 class GameChannel < ApplicationCable::Channel
+
   def subscribed
     p params
     stream_from "room_#{params[:room]}_#{params[:player]}"
-  end
-
-  # def receive(data)
-  #   ActionCable.server.broadcast("room_#{params[:room]}", data)
-  # end
-
-  # 创建新的房间
-  def create_room(data)
-    player = params[:player]
-    Room.fetch(player)
   end
 
   # 加入房间，如果人满，则开始
@@ -20,7 +11,7 @@ class GameChannel < ApplicationCable::Channel
     player = params['player']
     game = Game.fetch(room)
 
-    if game.player_can_in?(player)
+    unless game.player_can_in?(player)
       raise '房间已经满了，无法进入'
     end
 
@@ -28,7 +19,7 @@ class GameChannel < ApplicationCable::Channel
     # 如果是重新进入的，则直接获取数据即可。
     # 通过 runing? 来判断是否需要进行初始化。
     if game.runing?
-      game[:players].each do |player|
+      game.players.each do |player|
         send_current(game, player)
       end
 
@@ -37,14 +28,18 @@ class GameChannel < ApplicationCable::Channel
 
     # 进行初始化
     game.set_player(player)
+
+    # 添加用户之后，如果这个时候刚好2人
     if game.player_ready?
-      if game.ready?
-        game[:players].each do |player|
-          send_current(game, player)
-        end
+      # 初始化
+      game.setup!
+
+      game.players.each do |one|
+        send_current(game, one)
       end
     else
-      broadcast(
+      ActionCable.server.broadcast(
+        "room_#{params[:room]}_#{player}",
         type: :waitting,
         error_code: 10002,
         error_msg: '等待其他选手进入'
@@ -53,17 +48,22 @@ class GameChannel < ApplicationCable::Channel
   end
 
   def send_current(game, player)
+    if player == game.player_ping
+      current_player_status = game.player_ping_status
+    else
+      current_player_status = game.player_pong_status
+    end
+
     ActionCable.server.broadcast(
       "room_#{params[:room]}_#{player}",
       type: :fetch_current,
-      players: game[:players],
-      cities: game[:cities],
-      cities_status: game[:cities_status],
-      hand_cards: game[player][:hand_cards].sort_by { |c| [c.city, c.value] },
-      leave_count: game[:leave_cards].count
+      players: game.players,
+      cities: game.cities,
+      cities_status: game.cities_status,
+      hand_cards: current_player_status[:hand_cards].sort_by { |c| [c.city, c.value] },
+      leave_count: game.leave_cards.count
     )
   end
-
 
   # 获取当前该显示的数据
   # room: 房间标记
@@ -72,16 +72,22 @@ class GameChannel < ApplicationCable::Channel
   # data: {"player"=>"xiaodong", "action"=>"fetch_current"}
   def fetch_current(data)
     room = params['room']
-    player = data['player'].to_sym
+    player = data['player']
     game = Game.fetch(room)
+
+    if player == game.player_ping
+      current_player_status = game.player_ping_status
+    else
+      current_player_status = game.player_pong_status
+    end
 
     broadcast(
       type: :fetch_current,
-      players: game[:players],
-      cities: game[:cities],
-      cities_status: game[:cities_status],
-      hand_cards: game[player][:hand_cards].sort_by { |c| [c.city, c.value] },
-      leave_count: game[:leave_cards].count
+      players: game.players,
+      cities: game.cities,
+      cities_status: game.cities_status,
+      hand_cards: current_player_status[:hand_cards].sort_by { |c| [c.city, c.value] },
+      leave_count: game.leave_cards.count
     )
   end
 
@@ -95,43 +101,53 @@ class GameChannel < ApplicationCable::Channel
     p data
 
     room = params['room']
-    player = data['player'].to_sym
+    player = data['player']
     game = Game.fetch(room)
     error_code = 1000
     error_msg = ''
 
+    raise '牌组抽完，游戏结束' if game.finished
+
+    if player == game.player_ping
+      current_player_status = game.player_ping_status
+      current_player = :player_ping
+    else
+      current_player_status = game.player_pong_status
+      current_player = :player_pong
+    end
+
     # 找出当前想要打出的牌
-    hand_cards = game[player][:hand_cards]
+    hand_cards = current_player_status[:hand_cards]
     using_card = hand_cards.find { |card| card.id == data['card_id'] }
 
     raise '请选择一张手牌' if using_card.blank?
 
     # 找出该牌对应的城市
     # {:xiaodong=>[], :other=>[], :recycle_bin=>[]}
-    city_status = game[:cities_status][using_card.city]
+    city_status = game.cities_status[using_card.city]
 
     # 判断是否是想丢弃 还是投资进去。
     if data['throw_away']
       city_status[:recycle_bin].push(using_card)
     else
       # 开始进行投资
-      city_last_card = city_status[player].last
+      city_last_card = city_status[current_player].last
 
       # 没下过牌，则可以下
       if city_last_card.blank?
-        city_status[player].push(using_card)
+        city_status[current_player].push(using_card)
       else
         # 如果是投资卡，上一张卡只能是投资卡
         if using_card.type == 'investment'
           if city_last_card.type == 'investment'
-            city_status[player].push(using_card)
+            city_status[current_player].push(using_card)
           else
             raise '该投资卡无法下'
           end
         else
           # 如果是普通牌，比较大小
           if using_card.value > city_last_card.value
-            city_status[player].push(using_card)
+            city_status[current_player].push(using_card)
           else
             raise '点数比上一张小，无法下'
           end
@@ -145,7 +161,7 @@ class GameChannel < ApplicationCable::Channel
     error_code = 10001
     error_msg = e.message
   ensure
-    check_and_update(error_code, error_msg, room, game, player)
+    check_and_update(error_code, error_msg, room, game, current_player_status)
   end
 
   # 从弃牌堆里 / 剩余的牌堆里 拿一张牌
@@ -158,34 +174,45 @@ class GameChannel < ApplicationCable::Channel
     error_msg = ''
     finished = false
 
+    if player == game.player_ping
+      current_player_status = game.player_ping_status
+      current_player = :player_ping
+    else
+      current_player_status = game.player_pong_status
+      current_player = :player_pong
+    end
+
     # 从城市堆取回一张
     if data['if_from_city']
-      city_status = game[:cities_status][data['city']]
+      city_status = game.cities_status[data['city']]
       current_card = city_status[:recycle_bin].pop
 
       if current_card.blank?
         error_code = 10001
         error_msg = '无牌可取'
       else
-        game[player][:hand_cards].push(current_card)
+        current_player_status[:hand_cards].push(current_card)
       end
     else
       # 从弃牌堆取回一张
-      current_card = game[:leave_cards].pop
+      current_card = game.leave_cards.pop
 
       if current_card.blank?
-        error_code = 10001
+        error_code = 10002
         error_msg = '牌组抽完，游戏结束'
         finished = true
+
+        game.finished = true
+        Game.update(room, game)
       else
-        game[player][:hand_cards].push(current_card)
+        current_player_status[:hand_cards].push(current_card)
       end
     end
 
-    check_and_update(error_code, error_msg, room, game, player, { finished: finished })
+    check_and_update(error_code, error_msg, room, game, current_player_status, { finished: finished })
   end
 
-  def check_and_update(error_code, error_msg, room, game, player, options = {})
+  def check_and_update(error_code, error_msg, room, game, current_player_status, options = {})
     if error_code > 10000
       broadcast(
         type: :fetch_current,
@@ -197,11 +224,11 @@ class GameChannel < ApplicationCable::Channel
 
       data = options.merge(
         type: :fetch_current,
-        players: game[:players],
-        cities: game[:cities],
-        cities_status: game[:cities_status],
-        hand_cards: game[player][:hand_cards].sort_by { |c| [c.city, c.value] },
-        leave_count: game[:leave_cards].count
+        players: game.players,
+        cities: game.cities,
+        cities_status: game.cities_status,
+        hand_cards: current_player_status[:hand_cards].sort_by { |c| [c.city, c.value] },
+        leave_count: game.leave_cards.count
       )
       broadcast(data)
     end
@@ -209,6 +236,6 @@ class GameChannel < ApplicationCable::Channel
 
   # 下发给用户的数据
   def broadcast(data)
-    ActionCable.server.broadcast("room_#{params[:room]}", data)
+    ActionCable.server.broadcast("room_#{params[:room]}_#{params[:player]}", data)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,24 +7,23 @@ class Game
     { name: '金字塔', color: '#03a9f4' },
   ]
 
-  attr_accessor :cities
-  attr_accessor :player_ping, :player_pong, :players,
-  attr_accessor :map_source, :room
-  attr_accessor :runing
+  attr_accessor :room
+  attr_accessor :cities, :cities_status
+  attr_accessor :player_ping, :player_pong, :players
+  attr_accessor :player_ping_status, :player_pong_status
+  attr_accessor :leave_cards
+  attr_accessor :runing, :finished
 
-  def initialize(player_ping, player_pong, cities = CITIES)
-    @cities = cities
-    @player_ping = player_ping
-    @player_pong = player_pong
-    # @room = generate_rand
-
-    # setup
+  def initialize(room)
+    @room = room
+    @players = []
+    @runing = false
+    @finished = false
   end
 
   def player_can_in?(player)
-    z_players = Array(@players)
-    if z_players.length >= 2
-      z_players.include?(player)
+    if @players.length >= 2
+      @players.include?(player)
     else
       true
     end
@@ -35,10 +34,11 @@ class Game
   end
 
   def set_player(player)
-    z_players = Array(@players)
-    if !z_players.include?(player)
+    if !@players.include?(player)
       @players << player
-      Game.update(self.room, self)
+
+      room_key = "room:#{@room}"
+      Rails.cache.write(room_key, self)
     end
   end
 
@@ -46,42 +46,43 @@ class Game
     Array(@players).length == 2
   end
 
-  def setup
+  def setup!
     # 初始化卡牌
+    @cities = CITIES
     cards = Card.init_cards(@cities)
+
+    @player_ping = @players[0]
+    @player_pong = @players[1]
 
     # 每人8张手牌
     player_ping_in_hands = cards.pop(8)
     player_pong_in_hands = cards.pop(8)
 
     # 每个城市的当前状态
-    cities_status = {}
+    @cities_status = {}
     @cities.each do |city|
-      cities_status[city[:name]] = {
-        "#{@player_ping}": [],  # 选手A下的牌
-        "#{@player_pong}": [],  # 选手B下的牌
-        "recycle_bin": []       # 回收队列的牌
+      @cities_status[city[:name]] = {
+        player_ping: [],  # 选手A下的牌
+        player_pong: [],  # 选手B下的牌
+        recycle_bin: []   # 回收队列的牌
       }
     end
 
-    @map_source = {
-      # "room": @room,
-      "players": [@player_ping, @player_pong],
-      "#{@player_ping}": {
-        hand_cards: player_ping_in_hands    # 手牌
-      },
-      "#{@player_pong}": {
-        hand_cards: player_pong_in_hands    # 手牌
-      },
-      leave_cards: cards, # 牌堆剩余的牌
-      cities: @cities,
-      cities_status: cities_status
+    @player_ping_status = {
+      name: @player_ping,
+      hand_cards: player_ping_in_hands  # 手牌
     }
-  end
 
-  # 是否已经包含2名选手
-  def ready?
-    @player_ping && @player_pong
+    @player_pong_status = {
+      name: @player_pong,
+      hand_cards: player_pong_in_hands  # 手牌
+    }
+
+    @leave_cards = cards # 牌堆剩余的牌
+    @runing = true
+
+    room_key = "room:#{@room}"
+    Rails.cache.write(room_key, self)
   end
 
   private
@@ -90,34 +91,16 @@ class Game
   end
 
   class << self
-    # Game.start("FoTuRF6f", "xiaodong", "other")
-    def start(room, player_ping, player_pong)
-      raise '用户出错' if room.blank? || player_ping.blank? || player_pong.blank?
-
-      game = self.new(player_ping, player_pong)
-
-      room_key = "room:#{room}"
-      Rails.cache.write(room_key, game.map_source)
-
-      puts JSON.pretty_generate game.map_source.as_json
-    end
-
     def fetch(room)
       room_key = "room:#{room}"
-      Rails.cache.fetch(room_key)
+      Rails.cache.fetch(room_key) do
+        new(room)
+      end
     end
 
     def update(room, game)
       room_key = "room:#{room}"
       Rails.cache.write(room_key, game)
-    end
-
-    # 进入房间，此时先不用初始化卡牌
-    def into(room)
-      room_key = "room:#{room}"
-      game = Rails.cache.fetch(room_key) do
-
-      end
     end
 
   end
